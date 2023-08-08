6a416cefba030951a45fc28b9be7d50ea336fdd7
sinfzx <sinfzx@sina.com>
2023-08-08 16:55:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,10 +2,10 @@ import { Bard } from "googlebard";
 let cookies = `__Secure-1PSID=ZQh-Y56ngZsWMXBy3Vgk-LY9cGRAkeNJXq-TVngSpqteWZIEZ-e-BN7gMfRtGlWzdg7LgA.`;
 let bot = new Bard(cookies);
 let conversationId = "bardchat"; // optional: to make it remember the conversation
-let res;
 export default {
-  async fetch(request, env) {
-    res = await bot.ask("What is my name?", conversationId);
+  async fetch(request) {
+    
+   const res = await bot.ask(req.body, conversationId);
     return res;
   }
 }
